708a1eb52ad49e28a9b320a84d817927ab00dcc0
Adele Soullier <adele.soullier@gmail.com>
2019-05-17 16:18:53 +0200

    added view of photos in index

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,7 @@
 # app/uploaders/photo_uploader.rb
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  def default_url
+    "/" + ["", "default.png"].compact.join('_')
+  end
 end
