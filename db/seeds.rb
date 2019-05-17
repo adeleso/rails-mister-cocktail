34eccea894587f1c2d715d7d78fcdbad834e0516
Adele Soullier <adele.soullier@gmail.com>
2019-05-17 15:08:32 +0200

    added photo to cocktail model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
 require 'open-uri'
 require 'json'
 
-response = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
-json = JSON.parse(response.read)
+# seeding cocktails
 
-json['drinks'].take(15).each do |h|
-  Ingredient.create({'name' => h['strIngredient1']})
+puts 'Cleaning Cocktails database...'
+Cocktail.destroy_all
+puts 'Cleaning Doses database...'
+Dose.destroy_all
+puts 'Cleaning Ingredients database...'
+Ingredient.destroy_all
+
+puts 'Creating Ingredients...'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+result = JSON.parse(open(url).read)
+result["drinks"].each { |drink| Ingredient.create(name: drink["strIngredient1"]) }
+
+puts 'Creating Cocktails...'
+url = 'https://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json'
+result = JSON.parse(open(url).read)
+result.each do |cocktail|
+  one = Cocktail.create(name: cocktail['name'])
+  cocktail['ingredients'].each do |item|
+    ing = Ingredient.find_by(name: item['ingredient'])
+    if ing.nil?
+      ing = Ingredient.create(name: item['ingredient'])
+    end
+    dose = Dose.create(description: "#{item['amount']} #{item['unit']}")
+    dose.cocktail = one
+    dose.ingredient = ing
+    dose.save
+  end
 end
+
+puts 'Finished!'
